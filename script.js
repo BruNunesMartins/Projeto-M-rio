4af2a633f90905583e98d06ec4f00cbc1e5bd542
BrunaMartins <brunet_nunes123@hotmail.com>
2024-02-04 20:11:17 -0300

    segundo projeto Mario com algumas modifições para treinamento

---
+++ script.js
@@ -0,0 +1,17 @@
+let formulario = document.querySelector(".fale-conosco")
+
+
+
+
+
+
+function cliqueiNoBotao(){
+formulario.style.left = "700px"
+
+
+
+}
+
+function sumirFormulario(){
+    formulario.style.left = "-320px"
+}
